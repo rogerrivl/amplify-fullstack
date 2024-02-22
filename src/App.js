d9a2be8c963889d44b0594a3c6616e79f4488c40
Roger Rivl <forcemobilestudios@gmail.com>
2024-02-21 22:28:26 -0600

    backend added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import "@aws-amplify/ui-react/styles.css";
+import { withAuthenticator,Button,Heading,Image,View,Card } from '@aws-amplify/ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Testing
-        </a>
-      </header>
-    </div>
-  );
+function App ({signOut}){
+  return(
+    <View className='App'>
+      <Card>
+        <Image src={logo} className='App-logo' alt='logo'/>
+        <Heading level={1}>We Now have Auth</Heading>
+      </Card>
+      <Button onClick={signOut}>Log Out</Button>
+    </View>
+  )
 }
-
-export default App;
+export default withAuthenticator(App);
